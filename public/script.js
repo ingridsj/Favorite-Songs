c1f762df65dee7745984899a1f2876cde5d70313
Ingrid Almeida <ingridsjalmeida@gmail.com>
2021-01-06 20:48:13 -0300

    add script menu

--- public/script.js
+++ public/script.js
@@ -12,15 +12,3 @@ document.querySelector('.closeModal').addEventListener("click", function() {
     modalOverlay.classList.remove('active')
     modalOverlay.querySelector("iframe").src = ""
 })
-
-//MENU RESPONSIVO
-let show = true
-
-const menuSection = document.querySelector(".menuSection")
-const menuToggle = menuSection.querySelector(".menuToggle")
-
-menuToggle.addEventListener("click", () => {
-    document.body.style.overflow = show ? "hidden" : "initial"
-    menuSection.classList.toggle("on", show)
-    show = !show
-})
